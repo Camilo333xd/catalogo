23b03310a0fc2dbe5ad59d4f7c7482f04fac68fb
Camilo333xd <115275361+Camilo333xd@users.noreply.github.com>
2024-07-26 16:38:38 -0500

    Update script.js

--- Proyecto/script.js
+++ Proyecto/script.js
@@ -1,16 +1,16 @@
 $(document).ready(function(){
     $('.productForm').on('submit', function(event){
-      event.preventDefault();  // Evitar el envío normal del formulario y la recarga de la página
+      event.preventDefault();  // evita que se envie el foprmulario y recargue la pag
       
       $.ajax({
-        url: 'add_to_cart.php',  // La URL del script PHP que procesa los datos
-        type: 'POST',  // El método de la solicitud
-        data: $(this).serialize(),  // Serializar los datos del formulario
+        url: 'add_to_cart.php',  //
+        type: 'POST',  // 
+        data: $(this).serialize(),  // Serializar datos
         success: function(response){
           console.log(response);
           // Parsear la respuesta JSON
           var data = JSON.parse(response);
-          // Actualizar el contador del carrito con la respuesta del servidor
+
           $('#cuenta-carrito').text(data.cart_count);
         },
         error: function(jqXHR, textStatus, errorThrown) {
